6cc7717b61c83c8f207ab363ace5abe9530ad489
Carlos Eduardo <carloseduardo.aulas@gmail.com>
2022-07-19 20:36:54 -0300

    FEAT: Lazy Loaded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { AboutComponent } from "./about/about.component";
 import { LoginComponent } from "./login/login.component";
 
 const routes: Routes = [
+  {
+    path: "courses",
+    loadChildren: () =>
+      import("./courses/courses.module").then((m) => m.CoursesModule),
+  },
   { path: "login", component: LoginComponent },
   { path: "about", component: AboutComponent },
 ];
